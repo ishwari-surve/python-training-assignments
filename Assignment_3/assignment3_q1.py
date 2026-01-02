d082cc83573d705351a6a2af7f7eef44f6febcdc
ishwari-surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-02 09:33:59 +0530

    Create assignment3_q1.py

---
+++ Assignment_3/assignment3_q1.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    return a * b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+result = multiply(x, y)
+print("Multiplication is:", result)
