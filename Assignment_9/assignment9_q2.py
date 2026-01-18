1bbe526ac113e1e9376f7c13f394054513574acc
ishwari-surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-18 12:50:16 +0530

    Create assignment9_q2.py

---
+++ Assignment_9/assignment9_q2.py
@@ -0,0 +1,9 @@
+#Q2. Function ChkGreater() that accepts two numbers and prints the greater number
+def ChkGreater(no1,no2):
+    if no1 > no2:
+        print("This number is greater :",no1)
+    else:
+        print("This number is greater :",no2)
+
+ChkGreater(10,20)
+
