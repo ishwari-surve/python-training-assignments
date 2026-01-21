fd915204f780e25c6ca162bd959f05b7eee9c940
ishwari-surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-21 23:24:33 +0530

    Check Palindrome Number_q5.py

---
+++ Assignment_11/Check Palindrome Number_q5.py
@@ -0,0 +1,20 @@
+#Check Palindrome Number
+
+def is_palindrome(num):
+    original = num
+    reverse = 0
+
+    while num != 0:
+        digit = num % 10
+        reverse = reverse * 10 + digit
+        num //= 10
+
+    return original == reverse
+
+
+number = int(input("Enter a number: "))
+
+if is_palindrome(number):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
