c2c03dcdf8291f2c9fe07d48b8900f57f2fea415
ishwari-surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-21 22:31:19 +0530

    Update assignment10_q1.py

--- Assignmentsnew_10/assignment10_q1.py
+++ Assignmentsnew_10/assignment10_q1.py
@@ -1,1 +1,8 @@
+#WAP TO ACCEPT ONE NUMBER AND PRINTS MULTIPLICATION TABLE OF THAT NUMBER.
 
+def print_table(no):
+    for i in range(1,11):
+        print(no*i,end=" ")
+
+num = int(input("Enter a number:"))
+print_table(num)
