3877668f22a84ab74392dff45223fce2b00eb989
ishwari-surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-22 08:04:13 +0530

    assignment13_q1.py

---
+++ Assignment_13/assignment13_q1.py
@@ -0,0 +1,7 @@
+#Program to calculate area of a rectangle
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+print("Area of rectangle:", area)
