f95dec2df1cf7bcd36cd181a8f1721aa31e50ad5
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-23 22:36:26 +0530

    Create assignment15_q2.py

---
+++ Assignment_15/assignment15_q2.py
@@ -0,0 +1,7 @@
+#2.Write a lambda function using filter() which accepts a list of numbers and returns a list of even numbers.
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+
+print(even_numbers)
