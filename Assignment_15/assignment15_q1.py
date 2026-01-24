efa6f9fdd8fd959e827e1518f4e74e26fe740f4b
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-24 07:51:11 +0530

    Update assignment15_q1.py

--- Assignment_15/assignment15_q1.py
+++ Assignment_15/assignment15_q1.py
@@ -1,7 +1,12 @@
 # 1.Write a lambda function using map() which accepts a list of numbers and returns a list of squares of each number.
 
-numbers = [1, 2, 3, 4, 5]
+def main():
+    Data = [1, 2, 3, 4, 5]
+    print("Actual Data is :", Data)
 
-squares = list(map(lambda x: x * x, numbers))
+    Square = list(map(lambda x: x * x, Data))
+    print("Squares are :", Square)
 
-print(squares)
+
+if __name__ == "__main__":
+    main()
