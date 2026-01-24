9b1fef3c88df2ba61361322aabbb8d0e1a57106e
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-24 09:37:23 +0530

    Add files via upload

---
+++ Assignment_17/main17_q1.py
@@ -0,0 +1,12 @@
+# main.py
+# This file imports Arithmetic module and uses its functions
+
+import Arithmetic
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", Arithmetic.Add(num1, num2))
+print("Subtraction:", Arithmetic.Sub(num1, num2))
+print("Multiplication:", Arithmetic.Mult(num1, num2))
+print("Division:", Arithmetic.Div(num1, num2))
