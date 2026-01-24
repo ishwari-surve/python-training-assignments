05e0e838e99ed5efae58b17c3946cb15fb967d1c
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-24 10:09:14 +0530

    Create assignment17_q8.py

---
+++ Assignment_17/assignment17_q8.py
@@ -0,0 +1,14 @@
+#Increasing number pattern
+
+def Pattern(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+def main():
+    num = int(input("Enter number: "))
+    Pattern(num)
+
+if __name__ == "__main__":
+    main()
