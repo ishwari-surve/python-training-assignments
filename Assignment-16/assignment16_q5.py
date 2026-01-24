6b76d4dd1fe2b3405922af3123c64fba99b7157e
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-24 09:06:51 +0530

    Create assignment16_q5.py

---
+++ Assignment-16/assignment16_q5.py
@@ -0,0 +1,11 @@
+#Display numbers from 10 to 1
+
+def DisplayReverse():
+    for i in range(10, 0, -1):
+        print(i, end=" ")
+
+def main():
+    DisplayReverse()
+
+if __name__ == "__main__":
+    main()
