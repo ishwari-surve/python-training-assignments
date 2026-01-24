ffb3a3f4200ecc12c7292b010f3745eae84898b0
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-01-24 23:52:18 +0530

    Create assignment19_q2.py

---
+++ Assignment_19/assignment19_q2.py
@@ -0,0 +1,10 @@
+#2.Lambda function – multiplication of two numbers
+
+multiply = lambda a, b: a * b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+result = multiply(x, y)
+
+print("Output:", result)
